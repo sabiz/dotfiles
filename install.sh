6b8b4c6ae0851798847f84ffab5867e87d79e38c
sAbIz <bellett@sabiz.jp>
2019-03-08 10:13:05 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -eu
 
+
+#Getting OS
+if [ "$(uname)" == 'Darwin' ];then
+    OS='Mac'
+else
+    OS='Linux'
+fi
+
 # Util ----------------------------------------
 mkdir -p ~/.util
 
@@ -12,3 +20,11 @@ mv git-prompt.sh ~/.util/.
 chmod a+x ~/.util/git-prompt.sh
 
 
+
+# link dotfiles -------------------------------
+if [ $OS = 'Mac' ];then
+    ln -s ~/dotfiles/.bashrc ~/.bash_profile
+    echo "source ~/.bash_profile" > ~/.bashrc
+else
+    ln -s ~/dotfiles/.bashrc ~/.bashrc
+fi
