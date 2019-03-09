89357cfad94dc501d612e01743571fe23613e011
sAbIz <bellett@sabiz.jp>
2019-03-09 10:54:43 +0900

    update install.sh exist check

--- install.sh
+++ install.sh
@@ -19,19 +19,20 @@ curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-
 mv git-prompt.sh ~/.util/.
 chmod a+x ~/.util/git-prompt.sh
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+[[ ! -e ~/.fzf ]] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-git clone https://github.com/b4b4r07/enhancd ~/.util/enhancd
+[[ ! -e ~/.util/enhancd ]] && git clone https://github.com/b4b4r07/enhancd ~/.util/enhancd
 
 # link dotfiles -------------------------------
 if [ $OS = 'Mac' ];then
-    ln -s ~/dotfiles/.bashrc ~/.bash_profile
+    ln -fs ~/dotfiles/.bashrc ~/.bash_profile
     echo "source ~/.bash_profile" > ~/.bashrc
 else
-    ln -s ~/dotfiles/.bashrc ~/.bashrc
+    ln -fs ~/dotfiles/.bashrc ~/.bashrc
 fi
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
+ln -fs ~/dotfiles/.vimrc ~/.vimrc
+ln -fs ~/dotfiles/.vim ~/.vim
 
+echo Done.
